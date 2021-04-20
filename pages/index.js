08d4d999b6eb8369c1a6fa76429a2e8e9c6beaa5
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-20 15:13:21 -0400

    begins creating Nav component, creates .env to store env variables, creates Request.js to utilize string interpolation to access the TMDB API with our API key

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
@@ -13,8 +15,10 @@ export default function Home() {
       <Header />
 
       {/*  Nav  */}
-
+      <Nav />
+      
       {/*  Results  */}
+      <Results />
     </div>
   );
 }
